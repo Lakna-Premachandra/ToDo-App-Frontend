4d0780a97872f0187431d9730b8d80eeee20b4be
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-05-25 15:44:16 +0530

    post inputs added

--- src/Components/Home/inputs.tsx
+++ src/Components/Home/inputs.tsx
@@ -1,27 +1,43 @@
 // import { IoIosAddCircle } from "react-icons/io";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { IToDo } from "../hooks/useToDo";
 
-export default function Inputs() {
-
+export default function Inputs({
+  addTodo,
+  handleChange,
+  postData,
+}: {
+  addTodo: (e: any) => void;
+  handleChange: (e: any) => void;
+  postData: IToDo;
+}) {
   return (
     <>
-      <form className="flex justify-center items-center gap-5 max-sm:flex-col max-w-5xl w-full p-4">
+      <form
+        onSubmit={addTodo}
+        className="flex justify-center items-center gap-5 max-sm:flex-col max-w-5xl w-full p-4"
+      >
         <input
+          onChange={handleChange}
           className="text-lg p-3 border rounded-full w-full shadow-inner focus:outline-none focus:shadow-md"
           type="text"
           placeholder="Enter a task"
-          name="todo"
+          name="todoName"
+          value={postData.todoName}
         />
         <input
-          className="text-lg p-3 border rounded-full w-full shadow-inner focus:outline-none focus:sadow-md"
+          onChange={handleChange}
+          className="text-lg p-3 border rounded-full w-full shadow-inner focus:outline-none focus:shadow-md"
           type="text"
           placeholder="Enter Description"
           name="description"
+          value={postData.description}
         />
         <button
           type="submit"
           className="bg-green-600 text-3xl hover:bg-green-800 rounded-md text-[whitesmoke] p-2 active:scale-90 max-sm:w-full flex justify-center"
         >
-          {/* <IoIosAddCircle /> */}
+          <AddCircleIcon />
         </button>
       </form>
     </>
