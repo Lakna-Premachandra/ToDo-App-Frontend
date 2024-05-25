91f456a6c33981834d11df10e3a0dc406b51a540
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-05-25 15:47:41 +0530

    home index added

--- src/Components/Home/todos.tsx
+++ src/Components/Home/todos.tsx
@@ -1,40 +1,50 @@
-function Todos() {
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { IToDo } from "../hooks/useToDo";
+
+export default function Todos({
+  todos,
+}: {
+  todos: IToDo[];
+}) {
   return (
     <>
-      <div className="list-none w-full  border border-red-500
-      grid grid-cols-2 gap-10
-      px-8 py-4">
-        {/* {todos.map((todo) => ( */}
-        <div>
-          <div className="bg-slate-100 p-4  flex justify-between items-center border-red-500
-          ">
-            <div className="mb-3">
-              <h1>hi</h1>
-              <p>hihi</p>
+      <div className="todo-list grid grid-cols-2 gap-8 w-full max-sm:grid-cols-1 p-4">
+        {todos.map((todo: any) => {
+          return (
+            <div className="todo-item flex  flex-col  w-full shadow  border-2 border-slate-200 max-w-[500px] p-6 rounded-xl max-sm:flex-col gap-4">
+              <div key={todo.id} className=" w-full">
+                <div className="flex gap-3 justify-between">
+                  <h2 className="text-xl font-bold tracking-wide">
+                    {todo.todoName}
+                  </h2>
+                  <button>
+                    <CheckBoxOutlineBlankIcon />
+                  </button>
+                </div>
+                <div className="w-full  break-words whitespace-normal">
+                  {todo.description}
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <button
+                  type="submit"
+                  className="bg-[#27739f] text-3xl hover:bg-[#406d88] rounded-md text-[whitesmoke] p-2 active:scale-90 max-sm:w-full flex justify-center"
+                >
+                  <EditIcon />
+                </button>
+                <button
+                  type="submit"
+                  className="bg-[#da3434] text-3xl hover:bg-[#ba4141] rounded-md text-[whitesmoke] p-2 active:scale-90 max-sm:w-full flex justify-center"
+                >
+                  <DeleteIcon />
+                </button>
+              </div>
             </div>
-            <div className="flex gap-3">
-              <button>Delete</button>
-              <button>Edit</button>
-            </div>
-          </div>
-        </div>
-        {/* ))} */}
-
-        <div>
-          <div className="flex justify-between items-center">
-            <div className="mb-3">
-              <h1>hi</h1>
-              <p>hihi</p>
-            </div>
-            <div className="flex gap-3">
-              <button>Delete</button>
-              <button>Edit</button>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
 }
-
-export default Todos;
