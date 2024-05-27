f81d0e31490cc0989267471bac2cd4be0a017a06
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-05-27 11:32:26 +0530

    minor changes

--- src/Components/modals/modal.tsx
+++ src/Components/modals/modal.tsx
@@ -1,5 +1,6 @@
 import { IToDo } from "../hooks/useToDo";
 import { ChangeEvent, FormEvent } from "react";
+import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
 
 export const Modal = ({
   handleClose,
@@ -19,7 +20,11 @@ export const Modal = ({
         onSubmit={editTodo}
         className="max-w-xl h-[400px] bg-slate-200 p-6 w-full flex flex-col gap-3 z-20 relative rounded-lg shadow-lg focus:outline-none"
       >
-        <h1 className="text-slate-900 text-lg font-bold">Edit My Todo </h1>
+        <div className="flex justify-left items-center gap-1">
+        <h1 className="text-slate-900 text-lg font-semibold tracking-wider py-2">Edit My Todo </h1> 
+        <button className="text-black   max-sm:w-full flex justify-center"><PlaylistAddCheckIcon/></button>
+        </div>
+        
         <input
           className="p-2 rounded-md border border-slate-300"
           type="text"
@@ -35,7 +40,7 @@ export const Modal = ({
           onChange={editHandler}
           value={editData.description}
         ></textarea>
-        <button className="mt-3 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        <button className="mt-3 p-2 bg-blue-800 text-white rounded-md hover:bg-blue-600 tracking-wider font-semibold">
           Save Changes
         </button>
         <button
